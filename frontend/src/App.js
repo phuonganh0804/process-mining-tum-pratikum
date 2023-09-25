d3d70c06982620875074ad3401338aa439ae2d96
Phuong Anh Ngo <ge59vis@tum.de>
2023-09-25 20:30:10 +0200

    update app.js app.css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import result from "./result.png";
 
 import React, { Component } from "react";
 
@@ -8,10 +9,26 @@ class App extends Component {
     this.state = {
       uploadedFile: null,
       algorithm: "",
+      dependency: "",
+      and: "",
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleUpload = this.handleUpload.bind(this);
     this.handleOption = this.handleOption.bind(this);
+    this.handleDependency = this.handleDependency.bind(this);
+    this.handleAnd = this.handleAnd.bind(this);
+  }
+
+  handleDependency(event) {
+    this.setState({
+      dependency: event.target.value,
+    });
+  }
+
+  handleAnd(event) {
+    this.setState({
+      and: event.target.value,
+    });
   }
 
   handleChange(event) {
@@ -35,10 +52,13 @@ class App extends Component {
     const data = new FormData();
     data.append("file", this.state.uploadedFile);
     data.append("algorithm", this.state.algorithm);
+    data.append("dependency", this.state.dependency);
+    data.append("and", this.state.and);
     const response = await fetch("/api/upload", {
       method: "post",
       body: data,
     });
+
     try {
       if (response.ok) {
         alert("File uploaded successfully");
@@ -54,41 +74,62 @@ class App extends Component {
   render() {
     return (
       <body>
-        <div>
-          <h1>Process Mining Tool</h1>
-          <form>
-            <label for="file">
-              Select your XES file:
-              <input
-                id="file"
-                type="file"
-                accept=".xes"
-                onChange={this.handleChange}
-              />
-            </label>
-            <label for="algorithm">
-              Mining algorithm:
-              <datalist id="algorithm" name="algorithm">
-                <option value="">--Please choose an option--</option>
-                <option value="Alpha Algorithm"></option>
-                <option value="Heuristic Miner"></option>
-              </datalist>
-              <input
-                name="algorithm"
-                list="algorithm"
-                onChange={this.handleOption}
-              />
-            </label>
-            <label for="submit">
-              <button id="submit" onClick={this.handleUpload}>
-                Upload
-              </button>
-            </label>
-          </form>
+        <h1>Process Mining Tool</h1>
+        <div class="parent">
+          <div class="input">
+            <form>
+              <label for="file">
+                Select your XES file:
+                <input
+                  id="file"
+                  type="file"
+                  accept=".xes"
+                  onChange={this.handleChange}
+                />
+              </label>
+              <label for="algorithm">
+                Algorithm:
+                <select
+                  id="algorithm"
+                  name="algorithm"
+                  onChange={this.handleOption}
+                  value={this.state.algorithm}
+                >
+                  <option value="">--Please choose an option--</option>
+                  <option value="Alpha Algorithm">Alpha Algorithm</option>
+                  <option value="Heuristic Miner">Heuristic Miner</option>
+                </select>
+              </label>
+              <label for="dependency">
+                Dependency threshold:
+                <input
+                  type="text"
+                  placeholder="0.5"
+                  value={this.state.dependency}
+                  onChange={this.handleDependency}
+                />
+              </label>
+              <label for="and">
+                AND-threshold:
+                <input
+                  type="text"
+                  placeholder="0.1"
+                  value={this.state.and}
+                  onChange={this.handleAnd}
+                />
+              </label>
+              <label for="submit">
+                <button id="submit" onClick={this.handleUpload}>
+                  Start Mining
+                </button>
+              </label>
+            </form>
+          </div>
+          <div class="output">
+            <img src={result} alt="result" width="100%" height="60%" />
+          </div>
           <footer>
-            <p>
-              <a href="http://www.freepik.com">Designed by Freepik</a>
-            </p>
+            <p></p>
           </footer>
         </div>
       </body>
